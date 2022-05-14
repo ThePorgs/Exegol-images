3cc234ea509879afd15560f40900b982c912d0ba
Shutdown <nwodtuhs+github@pm.me>
2022-05-14 19:27:10 +0200

    Adding PR 1323 to Impacket

--- sources/install.sh
+++ sources/install.sh
@@ -301,11 +301,12 @@ function install_impacket() {
   # 1289: [ntlmrelayx] LDAP attack: Add DNS records through LDAP
   # 1290: [ntlmrelayx] Adds the creation of a new machine account through SMB
   # 1291: [dacledit] New example script for DACL manipulation
+  # 1323: [owneredit.py] New example script to change an object's owner
   git config --global user.email "exegol@install.er"
   git config --global user.name "Exegol installer"
   # skipping 1090, makes DCSync happen twice
-  # prs="1090 1135 1154 1171 1177 1184 1201 1202 1224 1253 1256 1267 1270 1280 1288 1289 1290 1291"
-  prs="1135 1154 1171 1177 1184 1201 1202 1224 1253 1256 1267 1270 1280 1288 1289 1290 1291"
+  # prs="1090 1135 1154 1171 1177 1184 1201 1202 1224 1253 1256 1267 1270 1280 1288 1289 1290 1291 1323"
+  prs="1135 1154 1171 1177 1184 1201 1202 1224 1253 1256 1267 1270 1280 1288 1289 1290 1291 1323"
   for pr in $prs; do git fetch origin pull/$pr/head:pull/$pr && git merge --strategy-option theirs --no-edit pull/$pr; done
   python3 -m pip install .
   cp -v /root/sources/grc/conf.ntlmrelayx /usr/share/grc/conf.ntlmrelayx
