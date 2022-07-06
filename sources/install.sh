40992075d6d56f9bba1ef5a6ceae5a997f7e267a
Wlayzz <34021743+wlayzz@users.noreply.github.com>
2022-07-06 16:41:45 +0200

    fixing python dependencies

--- sources/install.sh
+++ sources/install.sh
@@ -424,6 +424,7 @@ function install_empire() {
   colorecho "Applying Exegol specific patch"
   git apply /root/sources/patches/empire_install_sh_patch.diff
   ./setup/install.sh
+  python3 -m pip install .
   # Changing password
   sed -i 's/password123/exegol4thewin/' /opt/tools/Empire/empire/server/config.yaml
 }
