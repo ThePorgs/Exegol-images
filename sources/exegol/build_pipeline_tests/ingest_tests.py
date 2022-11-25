9c9d7298a20b4260a444b2600e085298d8ac9620
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2022-11-25 14:10:12 +0100

    Adding ingest_tests.py to JSONify the CI/CD tests

---
+++ sources/exegol/build_pipeline_tests/ingest_tests.py
@@ -0,0 +1,27 @@
+import json
+
+MAX_JOBS = 250
+
+
+# Dividing all_tests list into chunks of size n
+def divide_tests(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+# Reading all test commands and converting to JSON structure
+infile_path = "all_commands.txt"
+with open(infile_path, 'r') as f:
+    all_tests = f.read().splitlines()
+    print(f"[+] Read all tests from {infile_path}")
+    d = {'tests': list(divide_tests(all_tests, MAX_JOBS))}
+    print(f"[+] Divided into chunks of size {MAX_JOBS}")
+    json_tests = json.dumps(d, indent=4)
+    print("[+] Converted to JSON structure")
+
+
+# Writing to tests.json
+outfile_path = "tests.json"
+with open(outfile_path, "w") as outfile:
+    outfile.write(json_tests)
+    print(f"[+] JSON structure written to file {outfile_path}")
