7e2884a9fbc71f351fe103a8164827aa2da383a7
Alexis Martin <alexis.martin3190@gmail.com>
2023-03-09 23:44:39 +0100

    Randomise default ports + fix install war bug

--- sources/guacamole/install.sh
+++ sources/guacamole/install.sh
@@ -2,7 +2,6 @@
 
 # Set variables
 GUACVERSION="1.5.0"
-MCJVER="8.0.26"
 TOMCAT="tomcat9 tomcat9-admin tomcat9-common tomcat9-user"
 SERVER="http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/${GUACVERSION}"
 TOMCAT_HOME="/root/.tomcat"
@@ -20,6 +19,8 @@ ${TOMCAT}
 wget -q --show-progress -O guacamole-server-${GUACVERSION}.tar.gz ${SERVER}/source/guacamole-server-${GUACVERSION}.tar.gz
 tar -xzf guacamole-server-${GUACVERSION}.tar.gz
 
+mkdir -p /etc/guacamole/{extensions,lib}
+
 # Custom style
 mkdir custom-war
 cd custom-war
@@ -27,14 +28,10 @@ wget -q --show-progress -O guacamole-${GUACVERSION}.war ${SERVER}/binary/guacamo
 unzip guacamole-${GUACVERSION}.war
 rm guacamole-${GUACVERSION}.war
 echo '<link rel="stylesheet" href="theme.css">' >> index.html
-mv ../theme.css ./
+cp ../theme.css ./
 zip -r /etc/guacamole/guacamole.war *
 cd ..
 rm -rf custom-war
-
-# TODO: /etc/guacamole/guacamole.war disapear at the end of install - Weird
-
-mkdir -p /etc/guacamole/{extensions,lib}
 
 # Install guacamole-server
 cd guacamole-server-${GUACVERSION}
@@ -56,22 +53,22 @@ ln -sf /etc/guacamole/guacamole.war ${TOMCAT_HOME}/webapps/ROOT.war
 cat >> /etc/guacamole/guacd.conf <<- "EOF"
 [server]
 bind_host = 127.0.0.1
-bind_port = 4822
+bind_port = 5665
 EOF
 
 # Add login mapping
 cp user-mapping.xml /etc/guacamole/
+cp guacamole.properties /etc/guacamole/
 
 # Install kde plasma desktop
-apt install -y kde-plasma-desktop dbus-x11
+apt install -y mate-desktop-environment-core
 update-alternatives --set x-session-manager /usr/bin/startplasma-x11
 
 # Install RDP server
 apt install -y xrdp
 pkill xrdp
 
-sed -i "s/EnableSyslog=1/EnableSyslog=0/g" /etc/xrdp/sesman.ini
-sed -i "s#port=3389#port=tcp://127.0.0.1:3389##" /etc/xrdp/xrdp.ini # RDP localhost
+sed -i "s#port=3389#port=tcp://127.0.0.1:5225##" /etc/xrdp/xrdp.ini # RDP localhost
 
 apt-get remove -y bluedevil bluez && apt autoremove -y # https://github.com/jriddell/kdeneon-docker/issues/1
 
@@ -79,14 +76,14 @@ apt-get remove -y bluedevil bluez && apt autoremove -y # https://github.com/jrid
 echo -e "exegol4thewin\nexegol4thewin" | passwd root
 
 # Set default wallpaper
-kwriteconfig5 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "/workspace/wallpaper.png"
+kwriteconfig5 --file "/root/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "/workspace/wallpaper.png"
 
 # Dark mode
 ## TODO : FIX
-kwriteconfig5 --file kdeglobals --group "General" --key "ColorScheme" "BreezeDark"
-kwriteconfig5 --file kdeglobals --group "General" --key "Name" "BreezeDark"
-kwriteconfig5 --file kdeglobals --group "General" --key "Style" "BreezeDark"
-kwriteconfig5 --file kdeglobals --group "Icons" --key "Theme" "BreezeDark"
+kwriteconfig5 --file kdeglobals --group "General" --key "ColorScheme" "Breeze Dark"
+kwriteconfig5 --file kdeglobals --group "General" --key "Name" "Breeze Dark"
+kwriteconfig5 --file kdeglobals --group "General" --key "Style" "Breeze Dark"
+kwriteconfig5 --file kdeglobals --group "Icons" --key "Theme" "Breeze Dark"
 
 # Cleanup
 #rm -rf guacamole-*
@@ -101,7 +98,6 @@ pkill xrdp-sesman
 
 # Unset variables
 unset GUACVERSION
-unset MCJVER
 unset TOMCAT
 unset SERVER
 unset TOMCAT_HOME
--- sources/guacamole/restart-services.sh
+++ sources/guacamole/restart-services.sh
@@ -3,5 +3,6 @@
 pkill guacd
 service guacd start
 service xrdp restart
+service xrdp-sesman stop
 JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64 /root/.tomcat/bin/shutdown.sh
 JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64 /root/.tomcat/bin/startup.sh
