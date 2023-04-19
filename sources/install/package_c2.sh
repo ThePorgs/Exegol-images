7bba91e51ea23dd17a64fdccfa61c1103ef9bd78
QU35T <qu35t3190@gmail.com>
2023-04-19 20:14:00 +0200

    [DEBUG] - Fix metasploit install (re bundle install)

--- sources/install/package_c2.sh
+++ sources/install/package_c2.sh
@@ -16,8 +16,11 @@ function package_c2() {
 function install_c2_apt_tools() {
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb -o msfinstall && chmod +x msfinstall && ./msfinstall    #fapt metasploit-framework
 
+    cd /opt/metasploit-framework/embedded/framework
+    bundle install
+
     #add-test-command "msfconsole --version" # Offensive framework
-    add-test-command "/opt/metasploit-framework/bin/msfconsole --version" # Offensive framework
+    add-test-command "ruby /opt/metasploit-framework/embedded/framework/msfconsole --version" # Offensive framework
     
     add-to-list "metasploit,https://github.com/rapid7/metasploit-framework,A popular penetration testing framework that includes many exploits and payloads"
 }
