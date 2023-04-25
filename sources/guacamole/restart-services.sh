5f9646dd13d7faa872387d9792b410179a86bb23
QU35T <qu35t3190@gmail.com>
2023-04-26 01:47:32 +0200

    Improve my work

--- sources/guacamole/restart-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkill guacd
-service guacd start
-service xrdp restart
-service xrdp-sesman stop
-JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64 /root/.tomcat/bin/shutdown.sh
-JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64 /root/.tomcat/bin/startup.sh
--- sources/install/guacamole_install.sh
+++ sources/install/guacamole_install.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
+# Author: The Exegol Project
+
+source common.sh
 
 # Set variables
-GUACVERSION="1.5.0"
-TOMCAT="tomcat9 tomcat9-admin tomcat9-common tomcat9-user"
+GUACVERSION="1.5.1"
 SERVER="http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/${GUACVERSION}"
 TOMCAT_HOME="/root/.tomcat"
 
-apt-get update
 export DEBIAN_FRONTEND=noninteractive
 
 # Install dependencies
-apt-get -y install build-essential libcairo2-dev libjpeg62-turbo-dev libpng-dev libossp-uuid-dev libavcodec-dev libavformat-dev libavutil-dev \
+fapt libcairo2-dev libjpeg62-turbo-dev libpng-dev libossp-uuid-dev libavcodec-dev libavformat-dev libavutil-dev \
 libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev \
 libvorbis-dev libwebp-dev libwebsockets-dev freerdp2-x11 libtool-bin ghostscript dpkg-dev wget crudini libc-bin \
-${TOMCAT}
+tomcat9 tomcat9-admin tomcat9-common tomcat9-user kde-plasma-desktop xrdp xorgxrdp dbus-x11
+#systemsettings qml-module-org-kde-newstuff
+
+# Set default wallpaper
+kwriteconfig5 --file "/root/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "/root/sources/assets/guacamole/wallpaper.png"
+#kwriteconfig5 --file kdeglobals --group General --key ColorScheme "Breeze"
+#kwriteconfig5 --file ~/.config/plasmarc --group Theme --key name breeze-dark
+
+# Setup workspace
+mkdir /tmp/guacamole
+cd /tmp/guacamole/
 
 # Download requirements
 wget -q --show-progress -O guacamole-server-${GUACVERSION}.tar.gz ${SERVER}/source/guacamole-server-${GUACVERSION}.tar.gz
@@ -28,7 +39,9 @@ wget -q --show-progress -O guacamole-${GUACVERSION}.war ${SERVER}/binary/guacamo
 unzip guacamole-${GUACVERSION}.war
 rm guacamole-${GUACVERSION}.war
 echo '<link rel="stylesheet" href="theme.css">' >> index.html
-cp ../theme.css ./
+cp /root/sources/assets/guacamole/theme.css ./
+cp /root/sources/assets/guacamole/logo.png ./images/logo.png
+sed -i "s#guac-tricolor.svg#logo.png#" ./1.guacamole.8c18237a4a94ee9845d9.css
 zip -r /etc/guacamole/guacamole.war *
 cd ..
 rm -rf custom-war
@@ -40,10 +53,7 @@ export CFLAGS="-Wno-error"
 make
 make install
 ldconfig
-
-# Configure guacamole
 cd ../
-rm -f /etc/guacamole/guacamole.properties
 
 # Create a new tomcat instance on port 1337
 tomcat9-instance-create ${TOMCAT_HOME}
@@ -56,48 +66,33 @@ bind_host = 127.0.0.1
 bind_port = 5665
 EOF
 
-# Add login mapping
-cp user-mapping.xml /etc/guacamole/
-cp guacamole.properties /etc/guacamole/
+# Configure guacamole
+rm -f /etc/guacamole/guacamole.properties
 
-# Install kde plasma desktop
-apt install -y mate-desktop-environment-core
+# Add login mapping
+cp /root/sources/assets/guacamole/user-mapping.xml /etc/guacamole/
+cp /root/sources/assets/guacamole/guacamole.properties /etc/guacamole/
+
 update-alternatives --set x-session-manager /usr/bin/startplasma-x11
 
 # Install RDP server
-apt install -y xrdp
-pkill xrdp
-
-sed -i "s#port=3389#port=tcp://127.0.0.1:5225##" /etc/xrdp/xrdp.ini # RDP localhost
-
-apt-get remove -y bluedevil bluez && apt autoremove -y # https://github.com/jriddell/kdeneon-docker/issues/1
+sed -i "s#port=3389#port=tcp://127.0.0.1:5225##" /etc/xrdp/xrdp.ini
 
 # Edit root password
 echo -e "exegol4thewin\nexegol4thewin" | passwd root
 
-# Set default wallpaper
-kwriteconfig5 --file "/root/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' "/workspace/wallpaper.png"
+cp /root/sources/assets/guacamole/desktop-start /opt/tools/bin
+cp /root/sources/assets/guacamole/desktop-stop /opt/tools/bin
+cp /root/sources/assets/guacamole/desktop-restart /opt/tools/bin
 
-# Dark mode
-## TODO : FIX
-kwriteconfig5 --file kdeglobals --group "General" --key "ColorScheme" "Breeze Dark"
-kwriteconfig5 --file kdeglobals --group "General" --key "Name" "Breeze Dark"
-kwriteconfig5 --file kdeglobals --group "General" --key "Style" "Breeze Dark"
-kwriteconfig5 --file kdeglobals --group "Icons" --key "Theme" "Breeze Dark"
+chmod +x /opt/tools/bin/desktop-*
 
-# Cleanup
-#rm -rf guacamole-*
-#rm -rf user-mapping.xml
-#rm -rf install.sh
+desktop-stop
 
-# Stop services
-service guacd stop
-JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-arm64 ${TOMCAT_HOME}/bin/shutdown.sh
-xrdp -k
-pkill xrdp-sesman
+# Remove tmp
+# rm -rf /tmp/guacamole
 
 # Unset variables
 unset GUACVERSION
-unset TOMCAT
 unset SERVER
 unset TOMCAT_HOME
