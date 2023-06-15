a14e30d37fe1771a4746e01c9f3a91ea9e80dfea
QU35T <qu35t3190@gmail.com>
2023-06-15 10:14:51 +0200

    Addition of a structure in the setup for better support

--- sources/install/guacamole_install.sh
+++ sources/install/guacamole_install.sh
@@ -35,10 +35,7 @@ wget -q --show-progress -O guacamole-${GUACVERSION}.war ${SERVER}/binary/guacamo
 unzip guacamole-${GUACVERSION}.war
 rm guacamole-${GUACVERSION}.war
 echo '<link rel="stylesheet" href="theme.css">' >> index.html
-cp /root/sources/assets/guacamole/theme.css ./
-cp /root/sources/assets/guacamole/logo.png ./images/logo.png
-cp /root/sources/assets/guacamole/logo.png ./images/logo-144.png
-cp /root/sources/assets/guacamole/logo.png ./images/logo-64.png
+cp -r /root/sources/assets/guacamole/custom-css/* ./
 sed -i "s#guac-tricolor.svg#logo.png#" ./1.guacamole.8c18237a4a94ee9845d9.css
 sed -i "s#Apache Guacamole#Exegol#" ./translations/en.json
 zip -r /etc/guacamole/guacamole.war *
@@ -68,10 +65,9 @@ EOF
 rm -f /etc/guacamole/guacamole.properties
 
 # Add login mapping
-cp /root/sources/assets/guacamole/user-mapping.xml /etc/guacamole/
+cp /root/sources/assets/guacamole/etc-srv/* /etc/guacamole/
 hashpass=$(echo -n "exegol4thewin"|md5sum|cut -d ' ' -f1)
 sed -i "s/CHANGEME/$hashpass/" /etc/guacamole/user-mapping.xml
-cp /root/sources/assets/guacamole/guacamole.properties /etc/guacamole/
 
 update-alternatives --set x-session-manager /usr/bin/startplasma-x11
 
@@ -81,9 +77,7 @@ sed -i "s#port=3389#port=tcp://127.0.0.1:5225##" /etc/xrdp/xrdp.ini
 # Edit root password
 echo -e "exegol4thewin\nexegol4thewin" | passwd root
 
-cp /root/sources/assets/guacamole/desktop-start /opt/tools/bin
-cp /root/sources/assets/guacamole/desktop-stop /opt/tools/bin
-cp /root/sources/assets/guacamole/desktop-restart /opt/tools/bin
+cp /root/sources/assets/guacamole/bin/* /opt/tools/bin
 
 chmod +x /opt/tools/bin/desktop-*
 
