3bd4afe43f187b0f3d9dc99669a139cdd0a00c62
clomic <clomic@users.noreply.github.com>
2023-07-18 16:04:42 +0200

    Update package_cloud.sh
    Correct cloudsploit directory

--- sources/install/package_cloud.sh
+++ sources/install/package_cloud.sh
@@ -66,7 +66,7 @@ function install_cloudsplaining() {
 function install_cloudsploit() {
     colorecho "Installing Cloudsploit"
     git -C /opt/tools/ clone --depth 1 https://github.com/aquasecurity/cloudsploit
-    cd /opt/cloudsploit && npm install && chmod +x index.js
+    cd /opt/tools/cloudsploit && npm install && chmod +x index.js
     add-aliases cloudsploit
     add-history cloudsploit
     add-test-command "cloudsploit -h"
