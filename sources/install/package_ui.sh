bae603634d0ec200c638713371a04263939e0aef
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-07-30 17:42:46 +0200

    Add VNC auth for KDE

--- sources/install/package_ui.sh
+++ sources/install/package_ui.sh
@@ -36,18 +36,18 @@ function install_kde() {
     echo "Installing Kde"
     fapt tigervnc-standalone-server novnc websockify kde-plasma-desktop dbus-x11
     mkdir ~/.vnc
-
-    cp /root/sources/assets/webui/config-mate.conf ~/.vnc/xstartup
-
+    cp /root/sources/assets/webui/xstartup.conf ~/.vnc/xstartup
     chmod u+x ~/.vnc/xstartup
 
-    cp /root/sources/assets/webui/bin/* /opt/tools/bin
+    # TODO: Remove me
+    echo 'exegol4thewin' | vncpasswd -f > $HOME/.vnc/passwd
 
+    cp /root/sources/assets/webui/bin/* /opt/tools/bin
     chmod +x /opt/tools/bin/desktop-*
 }
 
 function package_webui() {
     # install_xfce
-    install_mate
-    # install_kde
+    # install_mate
+    install_kde
 }
