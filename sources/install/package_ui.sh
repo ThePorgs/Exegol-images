8f3b3da5950161466aa8a804185c8b80ebf0e555
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-07-30 17:06:22 +0200

    Add mate desktop

--- sources/install/package_ui.sh
+++ sources/install/package_ui.sh
@@ -17,11 +17,21 @@ function install_xfce() {
 
     cp /root/sources/assets/webui/bin/* /opt/tools/bin
 
-    chmod +x /opt/tools/bin/xfce-desktop-*
+    chmod +x /opt/tools/bin/desktop-*
 }
 
 function install_mate() {
     echo "Installing Mate"
+    fapt tigervnc-standalone-server novnc websockify mate-desktop-environment dbus-x11
+    mkdir ~/.vnc
+
+    cp /root/sources/assets/webui/config-mate.conf ~/.vnc/xstartup
+
+    chmod u+x ~/.vnc/xstartup
+
+    cp /root/sources/assets/webui/bin/* /opt/tools/bin
+
+    chmod +x /opt/tools/bin/desktop-*
 }
 
 function package_webui() {
