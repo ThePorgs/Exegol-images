64e93a40f307b69e16e5c17363815c65f37e82be
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-07-31 18:25:26 +0200

    Add Theme + Icons

--- sources/install/package_ui.sh
+++ sources/install/package_ui.sh
@@ -6,10 +6,37 @@ source common.sh
 set -e
 
 function install_xfce() {
-    fapt tigervnc-standalone-server novnc websockify xfce4 dbus-x11
+    fapt xz-utils gzip
+    fapt tigervnc-standalone-server novnc websockify xfce4 dbus-x11 plank
     mkdir ~/.vnc
     cp /root/sources/assets/webui/xstartup.conf ~/.vnc/xstartup
     chmod u+x ~/.vnc/xstartup
+
+    # Debug Theme
+    cp /root/sources/assets/webui/Mc-OS-CTLina-XFCE-Dark.tar.xz ./
+    tar -xvf Mc-OS-CTLina-XFCE-Dark.tar.xz
+    mv Mc-OS-CTLina-XFCE-Dark McOS-CTLina
+    
+    # Debug Icons
+    cp /root/sources/assets/webui/papirus-icon-theme-20230601.tar.gz ./
+    gunzip papirus-icon-theme-20230601.tar.gz
+    tar -xvf papirus-icon-theme-20230601.tar
+
+
+    mkdir /root/.themes
+    cp -r ./McOS-CTLina /root/.themes/
+
+    # Appearance theme
+    xfconf-query -c xsettings -p /Net/ThemeName -s "McOS-CTLina"
+    
+    # Backgroup wallpaper
+    # cp wallpaper.png /usr/share/images/desktop-base/
+    # xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/last-image -s /root/sources/assets/webui/wallpaper.png
+    # xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor1/last-image -s /root/sources/assets/webui/wallpaper.png
+
+    # Icons
+    cp -r ./Papirus-Dark /usr/share/icons/Papirus-Dark
+    xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
 
     # TODO: Remove me
     echo 'exegol4thewin' | vncpasswd -f > $HOME/.vnc/passwd
@@ -28,6 +55,7 @@ function install_mate() {
     # TODO: Remove me
     echo 'exegol4thewin' | vncpasswd -f > $HOME/.vnc/passwd
     
+    gsettings set org.mate.background picture-filename /root/sources/assets/webui/wallpaper.png
     cp /root/sources/assets/webui/bin/* /opt/tools/bin
     chmod +x /opt/tools/bin/desktop-*
 }
@@ -47,7 +75,7 @@ function install_kde() {
 }
 
 function package_webui() {
-    # install_xfce
+    install_xfce
     # install_mate
-    install_kde
+    # install_kde
 }
