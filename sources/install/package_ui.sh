566b9ab8fbccd4f19376cbb62c0a9329cbb9b545
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-08-01 13:29:58 +0200

    Add cairo dock

--- sources/install/package_ui.sh
+++ sources/install/package_ui.sh
@@ -6,8 +6,7 @@ source common.sh
 set -e
 
 function install_xfce() {
-    fapt xz-utils
-    fapt tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer novnc websockify xfce4 dbus-x11 xfce4-dev-tools libglib2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev g++ build-essential
+    fapt tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer novnc websockify xfce4 dbus-x11 papirus-icon-theme cairo-dock
     mkdir ~/.vnc
     cp /root/sources/assets/webui/xstartup.conf ~/.vnc/xstartup
     chmod u+x ~/.vnc/xstartup
@@ -18,31 +17,14 @@ function install_xfce() {
     tar -xvf ./Mc-OS-CTLina-XFCE-Dark.tar.xz
     mv Mc-OS-CTLina-XFCE-Dark McOS-CTLina
     
-    # Debug Icons
-    fapt papirus-icon-theme
-    # cp /root/sources/assets/webui/papirus-icon-theme-20230601.tar.gz ./
-    # tar -zxvf papirus-icon-theme-20230601.tar.gz
-
-
     mkdir /root/.themes
     cp -r ./McOS-CTLina /root/.themes/
 
     # Backgroup wallpaper
     cp /root/sources/assets/webui/wallpaper.png /usr/share/backgrounds/xfce/
 
-    # Icons
-    # cp -r ./Papirus-Dark /usr/share/icons/Papirus-Dark
-    # cp -r ./Papirus /usr/share/icons/
-    # cp -r ./Papirus-Light /usr/share/icons/
-    # cp -r ./ePapirus /usr/share/icons/
-    # cp -r ./ePapirus-Dark /usr/share/icons/
     cp /root/sources/assets/webui/xsettings.xml /root/.vnc/xsettings.xml
 
-    git -C /tmp/ clone https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin.git
-    cd /tmp/xfce4-docklike-plugin
-    sh autogen.sh
-    make
-    make install
     # TODO: Remove me
     echo 'exegol4thewin' | vncpasswd -f > $HOME/.vnc/passwd
 
