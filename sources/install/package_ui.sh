e3f52ae6b546db30dbcc73fda7d860520bc05923
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-08-02 12:24:08 +0200

    Update and install intltool

--- sources/install/package_ui.sh
+++ sources/install/package_ui.sh
@@ -6,7 +6,8 @@ source common.sh
 set -e
 
 function install_xfce() {
-    fapt terminator firefox-esr
+    update
+    fapt terminator firefox-esr intltool
     fapt tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer novnc websockify xfce4 dbus-x11 papirus-icon-theme
     mkdir ~/.vnc
     cp /root/sources/assets/webui/configuration/xstartup.conf ~/.vnc/xstartup
@@ -27,16 +28,16 @@ function install_xfce() {
     #cp /root/sources/assets/webui/xsettings.xml /root/.vnc/xsettings.xml
 
     fapt xfce4-dev-tools libglib2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev g++ build-essential
-    #git -C /tmp clone https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin.git
-    #cd /tmp/xfce4-docklike-plugin
-    #sh autogen.sh --prefix=/tmp/
-    #make
-    #make install
-    #CUSTOM_PATH=`find /usr/lib/ -name "xfce*"|head -n1`
-    #echo $CUSTOM_PATH
-    #mv /tmp/lib/xfce4/panel/plugins/libdocklike.* $CUSTOM_PATH/panel/plugins
-    #mv /tmp/share/xfce4/panel/plugins/docklike.desktop /usr/share/xfce4/panel/plugins
-    fapt xfce4-docklike-plugin
+    git -C /tmp clone https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin.git
+    cd /tmp/xfce4-docklike-plugin
+    sh autogen.sh --prefix=/tmp/
+    make
+    make install
+    CUSTOM_PATH=`find /usr/lib/ -name "xfce*"|head -n1`
+    echo $CUSTOM_PATH
+    mv /tmp/lib/xfce4/panel/plugins/libdocklike.* $CUSTOM_PATH/panel/plugins
+    mv /tmp/share/xfce4/panel/plugins/docklike.desktop /usr/share/xfce4/panel/plugins
+    #fapt xfce4-docklike-plugin
     cp -rv /tmp/share/locale/* /usr/share/locale
     
     # TODO: Remove me
