f9fc8eac9217aa379ff3caa5fa1e090360ff742d
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-09-06 19:56:28 +0200

    setting default dark theme for BurpSuite

--- sources/install/package_web.sh
+++ sources/install/package_web.sh
@@ -684,7 +684,9 @@ function install_burpsuite() {
     mkdir /opt/tools/BurpSuiteCommunity
     burp_version=$(curl -s "https://portswigger.net/burp/releases#community" | grep -P -o "\d{4}-\d-\d" | head -1 | tr - .)
     wget "https://portswigger.net/burp/releases/download?product=community&version=$burp_version&type=Jar" -O /opt/tools/BurpSuiteCommunity/BurpSuiteCommunity.jar
-    # FIXME: set up the dark theme right away?
+    # TODO: two lines below should set up dark theme as default, does it work?
+    mkdir -p /root/.BurpSuite/
+    cp -v /root/sources/assets/burpsuite/UserConfigCommunity.json /root/.BurpSuite/UserConfigCommunity.json
     # FIXME: add burp certificate to embedded firefox and chrome?
     # TODO: change Burp config to allow built-in browser to run
     add-aliases burpsuite
