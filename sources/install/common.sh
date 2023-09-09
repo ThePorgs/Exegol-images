ca995550b78b3b78e437683cb0a44ad8cda434c9
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-09-09 11:43:41 +0200

    adding initial structure for catch & retry

--- sources/install/common.sh
+++ sources/install/common.sh
@@ -6,8 +6,7 @@ export BLUE='\033[1;34m'
 export GREEN='\033[1;32m'
 export NOCOLOR='\033[0m'
 
-
-### Support functions
+### Echo functions
 
 function colorecho () {
     echo -e "${BLUE}[EXEGOL] $*${NOCOLOR}"
@@ -21,6 +20,65 @@ function criticalecho () {
 function criticalecho-noexit () {
     echo -e "${RED}[EXEGOL ERROR] $*${NOCOLOR}" 2>&1
 }
+
+### Catch & retry definitions
+
+function catch_and_retry() {
+  colorecho "Entering catch & retry function"
+  local retries=5
+  # wait time = scale_factor x (base_exponent ^ retry)
+  local scale_factor=2  # scaling factor
+  local base_exponent=4 # base of the exponent
+  # First retry: 2×41=2×4=82×41=2×4=8 seconds
+  # Second retry: 2×42=2×16=322×42=2×16=32 seconds
+  # Third retry: 2×43=2×64=1282×43=2×64=128 seconds
+  # Fourth retry: 2×44=2×256=5122×44=2×256=512 seconds
+  # Fifth retry: 2×45=2×1024=20482×45=2×1024=2048 seconds
+  local max_wait_time=600 # maximum wait time in seconds
+  local command="$@"
+
+  for ((i=1; i<=retries; i++)); do
+    echo "Attempt $i/$retries: Executing $command"
+    eval "$command"
+
+    # If command exits successfully, no need for more retries
+    if [[ $? -eq 0 ]]; then
+      echo "Command executed successfully."
+      return 0
+    fi
+
+    # Calculate the exponential backoff time
+    local wait_time=$((scale_factor * (base_exponent ** i)))
+
+    # Cap it at max_wait_time
+    wait_time=$(( wait_time > max_wait_time ? max_wait_time : wait_time ))
+
+    echo "Command failed. Waiting $wait_time seconds before retry..."
+    sleep "$wait_time"
+  done
+
+  echo "Command failed after $retries attempts."
+  return 1
+}
+
+function define_retry_function() {
+  local original_command=$1
+  eval "
+  function $original_command() {
+    catch_and_retry \"$original_command \$@\"
+  }
+  "
+}
+
+# Dynamically create wrappers
+commands_to_wrap=("curl" "wget" "apt-get" "git" "go")
+for cmd in "${commands_to_wrap[@]}"; do
+  colorecho "Defining new catch & retry function for $cmd"
+  define_retry_function "$cmd"
+done
+
+
+### Support functions
 
 function add-to-list() {
   echo $1 >> "/.exegol/installed_tools.csv"
