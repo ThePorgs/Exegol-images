a6797ce371c75500fc3ca56c3a78fdda391c14d0
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-09-14 00:42:33 +0200

    Replacing fapt by apt-fast in cnr list

--- sources/install/common.sh
+++ sources/install/common.sh
@@ -2,7 +2,7 @@
 # Author: The Exegol Project
 
 # Functions and commands that will be retried multiple times to counter random network issues when building
-CATCH_AND_RETRY_COMMANDS=("curl" "wget" "fapt" "git" "go" "git" "apt-get")
+CATCH_AND_RETRY_COMMANDS=("curl" "wget" "apt-fast" "git" "go" "git" "apt-get")
 
 export RED='\033[1;31m'
 export BLUE='\033[1;34m'
