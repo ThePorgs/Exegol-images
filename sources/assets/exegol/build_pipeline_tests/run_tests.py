bdc2a8520a0236348485f5566a578cb2cb1ed9c9
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-09-22 20:51:38 +0200

    fixing run_tests.py timing out (v2)

--- sources/assets/exegol/build_pipeline_tests/run_tests.py
+++ sources/assets/exegol/build_pipeline_tests/run_tests.py
@@ -1,104 +1,95 @@
 import asyncio
 import tempfile
+import subprocess
+from concurrent.futures import TimeoutError as FuturesTimeoutError
 
-red = "\033[1;31m"
-green = "\033[1;32m"
-yellow = "\033[33m"
-blue = "\033[1;34m"
-clear = "\033[0m"
+COMMANDS_FILE = "/.exegol/build_pipeline_tests/all_commands.sorted.txt"
+FAIL_LOG_FILE = "/.exegol/build_pipeline_tests/failed_commands.log"
+TIMEOUT_LOG_FILE = "/.exegol/build_pipeline_tests/timedout_commands.log"
+SUCCESS_LOG_FILE = "/.exegol/build_pipeline_tests/success_commands.log"
 
-# The file containing the list of commands
-commands_file = "/.exegol/build_pipeline_tests/all_commands.sorted.txt"
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YELLOW = "\033[33m"
+BLUE = "\033[1;34m"
+RESET = "\033[0m"
 
-# The file to store the logs of commands
-fail_log_file = "/.exegol/build_pipeline_tests/failed_commands.log"
-timeout_log_file = "/.exegol/build_pipeline_tests/timedout_commands.log"
-success_log_file = "/.exegol/build_pipeline_tests/success_commands.log"
+COMMAND_TIMEOUT = 20
+CONCURRENT_TASKS = 5
 
-# Read the commands from the file
-with open(commands_file) as f:
-    commands = f.readlines()
+class CommandRunner:
+    def __init__(self, parallel_tasks: int = 20):
+        self.sem = asyncio.Semaphore(parallel_tasks)
+        self.failed_commands = []
+        self.timedout_commands = []
 
-# Remove the newline characters from the commands
-commands = [x.strip() for x in commands]
+    async def run(self, commands: list[str]) -> None:
+        await asyncio.gather(*(self._run_command(command) for command in commands))
 
-# Initialize the variable to store the failed/timedout commands
-failed_commands = []
-timedout_commands = []
+    async def _run_command(self, command: str) -> None:
+        async with self.sem:
+            with tempfile.NamedTemporaryFile(mode='w') as temp:
+                temp.write(command)
+                temp.flush()
+                zsh_command = f"zsh -c 'autoload -Uz compinit; compinit; source ~/.zshrc; . {temp.name}'"
+                loop = asyncio.get_running_loop()
+                try:
+                    proc, stdout, stderr = await loop.run_in_executor(None, self._run_subprocess, zsh_command, COMMAND_TIMEOUT)
+                    if proc.returncode == 0:
+                        print(f"{GREEN}SUCCESS{RESET} - Running command: {command}")
+                        # Write the output of the successful command to the log file
+                        self._log_command(log_file=SUCCESS_LOG_FILE, command=command, stdout=stdout, stderr=stderr)
+                    else:
+                        # If the command fails, store it in the list of failed commands
+                        self.failed_commands.append(command)
+                        print(f"{RED}FAILURE{RESET} - Running command: {command}")
+                        self._log_command(log_file=FAIL_LOG_FILE, command=command, stdout=stdout, stderr=stderr)
+                except asyncio.exceptions.TimeoutError:
+                    # If the command timeout, store it in the list of failed commands
+                    self.timedout_commands.append(command)
+                    print(f"{YELLOW}TIMEOUT{RESET} - Running command: {command}")
+                    self._log_command(log_file=TIMEOUT_LOG_FILE, command=command)
 
-# Define a function to run a single command
-async def run_command(command):
-    # Create a temporary file for the command
-    with tempfile.NamedTemporaryFile(mode="w") as temp:
-        # Write the command to the temporary file
-        temp.write(command)
-        temp.flush()
+    @staticmethod
+    def _run_subprocess(zsh_command: str, timeout: int) -> tuple:
         try:
-            # Try to run the command in a zsh context
-            zsh_command = f"zsh -c 'autoload -Uz compinit; compinit; source ~/.zshrc; . {temp.name}'"
-            proc = await asyncio.create_subprocess_shell(
-                cmd=zsh_command,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE)
-            stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=20)
-            # check if the command was successful
-            if proc.returncode == 0:
-                print(f"{green}SUCCESS{clear} - Running command: {command}")
-                # Write the output of the successful command to the log file
-                with open(success_log_file, "a") as f:
-                    f.write(f"{blue}mCommand: {command}\n{clear}")
-                    f.write(f"{yellow}Standard output:\n")
-                    for line in stdout.decode().split("\n"):
-                        f.write(f"    {line}\n")
-                    f.write(f"{clear}")
-            else:
-                # If the command fails, store it in the list of failed commands
-                failed_commands.append(command)
-                print(f"{red}FAILURE{clear} - Running command: {command}")
-                # Write the output of the failed command to the log file
-                with open(fail_log_file, "a") as f:
-                    f.write(f"{blue}Failed command: {command}\n{clear}")
-                    if stderr:
-                        f.write(f"{yellow}Standard output:\n")
-                        for line in stderr.decode().split("\n"):
-                            f.write(f"    {line}\n")
-                        f.write(f"{clear}")
-                    f.write(f"{red}Standard error:\n")
-                    for line in stderr.decode().split("\n"):
-                        f.write(f"    {line}\n")
-                    f.write(f"{clear}")
-        except asyncio.TimeoutError as e:
-            # If the command timeout, store it in the list of failed commands
-            timedout_commands.append(command)
-            print(f"{yellow}TIMEOUT{clear} - Running command: {command}")
-            # Add the timed out command to the log file
-            with open(timeout_log_file, "a") as f:
-                f.write(f"{blue}Timed out command: {command}\n{clear}")
+            proc = subprocess.run(zsh_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
+            return proc, proc.stdout, proc.stderr
+        except subprocess.TimeoutExpired:
+            raise FuturesTimeoutError() from None
+
+    @staticmethod
+    def _log_command(log_file: str, command: str, stdout: bytes = None, stderr: bytes = None) -> None:
+        with open(log_file, 'a') as f:
+            f.write(f"COMMAND: {command}\n")
+            if stdout is not None: f.write(f"\tSTDOUT:\n{stdout.decode('utf-8', 'replace')}\n")
+            if stderr is not None: f.write(f"\tSTDERR:\n{stderr.decode('utf-8', 'replace')}\n")
+
+
+def read_commands(file: str) -> list[str]:
+    with open(file, 'r') as f:
+        return [cmd.strip() for cmd in f.readlines()]
 
 
 async def main():
-    # Read the commands and create a list of coroutine objects
-    coros = [run_command(command) for command in commands]
-    # Run the coroutines concurrently
-    await asyncio.gather(*coros)
+    runner = CommandRunner(parallel_tasks=CONCURRENT_TASKS)
+    commands = read_commands(COMMANDS_FILE)
+    await runner.run(commands)
 
+    if runner.failed_commands:
+        print(f"{YELLOW}The following commands failed:{RESET}")
+        for command in runner.failed_commands:
+            print(f"    {command}")
+    if runner.timedout_commands:
+        print(f"{YELLOW}The following commands timedout:{RESET}")
+        for command in runner.timedout_commands:
+            print(f"    {command}")
+        print(f"{YELLOW}Logs of failed commands are stored in{RESET} {FAIL_LOG_FILE}")
+        print(f"{YELLOW}Logs of timedout commands are stored in{RESET} {TIMEOUT_LOG_FILE}")
+        print(f"{YELLOW}Logs of success commands are stored in{RESET} {SUCCESS_LOG_FILE}")
+        exit(1)
+    else:
+        print("All commands succeeded.")
+        print(f"{YELLOW}Logs of success commands are stored in{RESET} {SUCCESS_LOG_FILE}")
 
-# Run the event loop
 asyncio.run(main())
-
-# Check if any of the commands failed
-if failed_commands:
-    print(f"{yellow}The following commands failed:{clear}")
-    for command in failed_commands:
-        print(f"    {command}")
-    print(f"{yellow}The following commands timedout:{clear}")
-    for command in timedout_commands:
-        print(f"    {command}")
-    print(f"{yellow}Logs of failed commands are stored in{clear} {fail_log_file}")
-    print(f"{yellow}Logs of timedout commands are stored in{clear} {timeout_log_file}")
-    print(f"{yellow}Logs of success commands are stored in{clear} {success_log_file}")
-    exit(1)
-else:
-    print("All commands succeeded.")
-    print(f"{yellow}Logs of success commands are stored in{clear} {success_log_file}")
-    exit(0)
