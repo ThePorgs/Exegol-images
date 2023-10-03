5e4f89cb55816feff756cd3ed6b66584a4cef1d3
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-10-03 10:40:35 +0200

    Update scrtdnsdump details

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -1106,14 +1106,14 @@ function install_freeipscanner() {
     add-to-list "freeipscanner,https://github.com/scrt/freeipscanner,A simple bash script to enumerate stale ADIDNS entries"
 }
 
-# function install_scrtdnsdump() {
-#     # CODE-CHECK-WHITELIST=add-aliases
-#     colorecho "Installing scrtdnsdump"
-#     pipx install git+https://github.com/scrt/scrtdnsdump
-#     add-history scrtdnsdump
-#     add-test-command "scrtdnsdump --help"
-#     add-to-list "scrtdnsdump,https://github.com/scrt/scrtdnsdump,Enumeration and exporting of all DNS records in the zone for recon purposes of internal networks"
-# }
+function install_scrtdnsdump() {
+    # CODE-CHECK-WHITELIST=add-aliases
+    colorecho "Installing scrtdnsdump"
+    pipx install git+https://github.com/scrt/scrtdnsdump
+    add-history scrtdnsdump
+    add-test-command "scrtdnsdump --help"
+    add-to-list "scrtdnsdump,https://github.com/scrt/scrtdnsdump,Enumeration and exporting of all DNS records in the zone for recon purposes of internal networks"
+}
 
 # Package dedicated to internal Active Directory tools
 function package_ad() {
@@ -1204,5 +1204,5 @@ function package_ad() {
     install_extractbitlockerkeys   # Extract Bitlocker recovery keys from all the computers of the domain
     install_LDAPWordlistHarvester
     install_freeipscanner
-    # install_scrtdnsdump
+    # install_scrtdnsdump          # This tool is a fork of adidnsdump (https://github.com/dirkjanm/adidnsdump). We are currently waiting to see if a PR will be made.
 }
