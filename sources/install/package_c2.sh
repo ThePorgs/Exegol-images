d43e74af0cd1ee5096c4fbd2e2214996fccc5158
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-10-19 00:23:32 +0200

    basis for Empire install (amd64 only atm)

--- sources/install/package_c2.sh
+++ sources/install/package_c2.sh
@@ -72,14 +72,48 @@ function install_sliver() {
     add-to-list "sliver,https://github.com/BishopFox/sliver,Open source / cross-platform and extensible C2 framework"
 }
 
+function install_empire() {
+    colorecho "Installing Empire"
+    wget -O /tmp/packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
+    dpkg -i /tmp/packages-microsoft-prod.deb
+    fapt apt-transport-https libicu-dev xclip zip
+    # Installing .NET 6.0 SDK
+    wget -O /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+    chmod +x /tmp/dotnet-install.sh
+    /tmp/dotnet-install.sh --channel 6.0
+    # Powershell is required, but should be installed in package_ad.sh already
+    git -C /opt/tools/ clone --recursive https://github.com/BC-SECURITY/Empire
+    cd /opt/tools/Empire || exit
+    python3 -m venv ./venv
+    source ./venv/bin/activate
+    if [[ $(uname -m) = 'x86_64' ]]
+    then
+      pip3 install .
+    elif [[ $(uname -m) = 'aarch64' ]]
+    then
+      # for ARM64, pip install doesn't work because of donut-shellcode not supporting this arch (https://github.com/TheWover/donut/issues/139)
+      criticalecho-noexit "This installation function doesn't support architecture $(uname -m)" && return
+    else
+      criticalecho-noexit "This installation function doesn't support architecture $(uname -m)" && return
+    fi
+    deactivate
+    sed -i 's/use: mysql/use: sqlite/g' empire/server/config.yaml
+    sed -i 's/password: password123/password: exegol4thewin/g' empire/server/config.yaml
+    cp -r -v ./empire/server/data/Invoke-Obfuscation /opt/tools/powershell/7/Modules/
+    add-aliases empire
+    add-history empire
+    add-test-command "ps-empire server --help"
+    add-test-command "ps-empire client --help"
+    add-to-list "empire,https://github.com/BC-SECURITY/Empire,post-exploitation and adversary emulation framework"
+}
+
 # Package dedicated to command & control frameworks
 function package_c2() {
     set_cargo_env
     set_go_env
     set_ruby_env
     set_python_env
-    # install_empire                # Exploit framework FIXME
-    # install_starkiller            # GUI for Empire, commenting while Empire install is not fixed
+    install_empire                  # Post-ex and adversary simulation framework
     install_pwncat                  # netcat and rlwrap on steroids to handle revshells, automates a few things too
     install_metasploit              # Offensive framework
     install_routersploit            # Exploitation Framework for Embedded Devices
