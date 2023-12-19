04a26f4fe32040d98e524ea1af28a2b283cd583f
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2023-12-19 21:48:44 +0100

    Postponing 2023/12/18 temp fix on gmsadumper

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -908,8 +908,8 @@ function install_gmsadumper() {
     python3 -m venv ./venv
     source ./venv/bin/activate
     pip3 install -r requirements.txt
-    # same as https://github.com/franc-pentest/ldeep/issues/41
-    local temp_fix_limit="2023-12-18"
+    # https://github.com/micahvandeusen/gMSADumper/issues/12
+    local temp_fix_limit="2024-01-20"
     if [[ "$(date +%Y%m%d)" -gt "$(date -d $temp_fix_limit +%Y%m%d)" ]]; then
       criticalecho "Temp fix expired. Exiting."
     else
