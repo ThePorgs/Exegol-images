f75738613751246597b3963d0054ed8e0fb7ba74
QU35T-code <github@qu35t-mail.simplelogin.com>
2024-02-12 11:54:47 +0100

    Update package_cracking

--- sources/install/package_cracking.sh
+++ sources/install/package_cracking.sh
@@ -18,15 +18,15 @@ function install_cracking_apt_tools() {
     add-test-command "pdfcrack --version"                               # PDF cracker
     add-test-command "bruteforce-luks -h |& grep 'Print progress info'" # Find the password of a LUKS encrypted volume
 
-    add-version "hashcat --version"
-    add-version "fcrackzip --version | awk '{print $3}'"
-    add-version "pdfcrack --version | awk '{print $3}'"
-    add-version "bruteforce-luks -h |& grep bruteforce-luks | head -n 1 | awk '{print $2}'"
+    local version=$(hashcat --version)
+    local version=$(fcrackzip --version | awk '{print $3}')
+    local version=$(pdfcrack --version | awk '{print $3}')
+    local version=$(bruteforce-luks -h |& grep bruteforce-luks | head -n 1 | awk '{print $2}')
 
-    add-to-list "hashcat,https://hashcat.net/hashcat,A tool for advanced password recovery"
-    add-to-list "fcrackzip,https://github.com/hyc/fcrackzip,Password cracker for zip archives."
-    add-to-list "pdfcrack,https://github.com/robins/pdfcrack,A tool for cracking password-protected PDF files"
-    add-to-list "bruteforce-luks,https://github.com/glv2/bruteforce-luks,A tool to help recover encrypted LUKS2 containers"
+    add-to-list "hashcat,https://hashcat.net/hashcat,A tool for advanced password recovery,$version"
+    add-to-list "fcrackzip,https://github.com/hyc/fcrackzip,Password cracker for zip archives.,$version"
+    add-to-list "pdfcrack,https://github.com/robins/pdfcrack,A tool for cracking password-protected PDF files,$version"
+    add-to-list "bruteforce-luks,https://github.com/glv2/bruteforce-luks,A tool to help recover encrypted LUKS2 containers,$version"
 }
 
 function install_john() {
@@ -37,10 +37,10 @@ function install_john() {
     yes|cpan install Compress::Raw::Lzma
     add-aliases john-the-ripper
     add-history john-the-ripper
-    add-version "john --help | head -n 1 | awk '{print $4}'"
+    local version=$(john --help | head -n 1 | awk '{print $4}')
     add-test-command "john --help"
     add-test-command "7z2john.pl|& grep 'Usage'"
-    add-to-list "john,https://github.com/openwall/john,John the Ripper password cracker."
+    add-to-list "john,https://github.com/openwall/john,John the Ripper password cracker.,$version"
 }
 
 function install_name-that-hash() {
@@ -49,7 +49,7 @@ function install_name-that-hash() {
     pipx install name-that-hash
     add-history name-that-hash
     add-test-command "nth --help"
-    add-to-list "name-that-hash,https://github.com/HashPals/Name-That-Hash,Online tool for identifying hashes."
+    add-to-list "name-that-hash,https://github.com/HashPals/Name-That-Hash,Online tool for identifying hashes.,$version"
 }
 
 function install_haiti() {
@@ -59,9 +59,9 @@ function install_haiti() {
     rvm use 3.2.2@default
     add-aliases haiti
     add-history haiti
-    add-version "haiti --version"
+    local version=$(haiti --version)
     add-test-command "haiti --help"
-    add-to-list "haiti,https://github.com/noraj/haiti,haiti is a A CLI tool (and library) to identify hash types (hash type identifier)."
+    add-to-list "haiti,https://github.com/noraj/haiti,haiti is a A CLI tool (and library) to identify hash types (hash type identifier).,$version"
 }
 
 function install_geowordlists() {
@@ -69,9 +69,9 @@ function install_geowordlists() {
     colorecho "Installing GeoWordlists"
     pipx install git+https://github.com/p0dalirius/GeoWordlists
     add-history geowordlists
-    add-version "geowordlists --help | head -n 1 | awk '{print $2}'"
+    local version=$(geowordlists --help | head -n 1 | awk '{print $2}')
     add-test-command "geowordlists --help"
-    add-to-list "geowordlists,https://github.com/p0dalirius/GeoWordlists,tool to generate wordlists of passwords containing cities at a defined distance around the client city."
+    add-to-list "geowordlists,https://github.com/p0dalirius/GeoWordlists,tool to generate wordlists of passwords containing cities at a defined distance around the client city.,$version"
 }
 
 function install_pkcrack() {
@@ -86,7 +86,7 @@ function install_pkcrack() {
     ln -s /opt/tools/pkcrack/bin/zipdecrypt /opt/tools/bin
     add-history pkcrack
     add-test-command 'pkcrack --help |& grep "Usage"'
-    add-to-list "pkcrack,https://github.com/keyunluo/pkcrack,tool to generate wordlists of passwords containing cities at a defined distance around the client city"
+    add-to-list "pkcrack,https://github.com/keyunluo/pkcrack,tool to generate wordlists of passwords containing cities at a defined distance around the client city,$version"
 }
 
 # Package dedicated to offline cracking/bruteforcing tools
