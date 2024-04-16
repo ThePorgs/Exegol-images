d436f5826bfe5bc6e2635a59ca525111af6ffd45
QU35T-code <51704860+QU35T-code@users.noreply.github.com>
2024-04-16 22:16:37 +0200

    Improve CCC adding temp-fix check (#335)

--- sources/code_compliance_check.py
+++ sources/code_compliance_check.py
@@ -1,11 +1,11 @@
 import os
 import re
 import sys
+import datetime
 
-blue = "\033[1;34m"
-magenta = "\033[1;35m"
-clear = "\033[0m"
-
+BLUE = "\033[1;34m"
+MAGENTA = "\033[1;35m"
+CLEAR = "\033[0m"
 
 def is_whitelisted(check_function_name, function_content):
     """Check if the function content is whitelisted based on # CODE-CHECK-WHITELIST directive."""
@@ -18,46 +18,54 @@ def is_whitelisted(check_function_name, function_content):
                 return True
     return False
 
-
 def get_functions_with_content(filename):
     """Extract functions starting with 'install_' and their content."""
     with open(filename, 'r') as file:
         content = file.read()
-    # Regex to retrieve function name and content
-    pattern = r'function\s+(install_\S+)\(\)\s+({[^}]*\n})'
-    return re.findall(pattern, content, re.DOTALL)
-
+    return re.findall(r'function\s+(install_\S+)\(\)\s+({[^}]*\n})', content, re.DOTALL)
 
 def contains_target_function(function_content, target_function):
     """Check if the function content calls the target function (and that the call is not commented)"""
-    for line in function_content.split("\n"):
-        stripped_line = line.strip()
-        if target_function in stripped_line and not stripped_line.startswith("#"):
-            return True
-    return False
+    return any(target_function in line.strip() and not line.strip().startswith("#")
+               for line in function_content.split("\n"))
 
-
-def is_code_compliant(check_function_name):
+def is_code_compliant(check_type, check_function_name=None):
+    """Checks the compliance of installation scripts based on the requested check type."""
     compliant = True
-    # Browse all files in the installation folder
+    today = datetime.date.today()
+    # Walks through all files in the specified installation directory
     for root, _, files in os.walk("./sources/install/"):
         for file in files:
-            # Parse only files that have the package prefix and are sh files
             if file.startswith("package_") and file.endswith(".sh"):
                 file_path = os.path.join(root, file)
-                # Get function name and content
+                # Extracts functions and their content
                 for func_name, func_content in get_functions_with_content(file_path):
-                    # Raising logs and noncompliance if target function is not in exclusion list for this function, and is not called in its content
-                    if not contains_target_function(func_content, check_function_name) and not is_whitelisted(check_function_name, func_content):
-                        print(f"{magenta}File: {file_path}{clear}")
-                        print(f"{blue}Function: {func_name}{clear}")
-                        print(func_content)
-                        compliant = False
+                    if check_type == "temp-fix":
+                        # Looks for a temporary fix expiration date within the function content
+                        date_found = re.search(r'temp_fix_limit="(\d{4}-\d{2}-\d{2})"', func_content)
+                        if date_found and datetime.datetime.strptime(date_found.group(1), '%Y-%m-%d').date() < today:
+                            print(f"{MAGENTA}File: {file_path}{CLEAR}")
+                            print(f"{BLUE}Function: {func_name}{CLEAR}")
+                            print(func_content)
+                            compliant = False
+                    elif check_type == "compliance":
+                        # Checks if the target function is not called and is not whitelisted
+                        if not contains_target_function(func_content, check_function_name) and not is_whitelisted(check_function_name, func_content):
+                            print(f"{MAGENTA}File: {file_path}{CLEAR}")
+                            print(f"{BLUE}Function: {func_name}{CLEAR}")
+                            print(func_content)
+                            compliant = False
     return compliant
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 code_compliance_check.py <check_function_name>")
+        print("Usage: python3 code_compliance_check.py <check_function_name|temp-fix>")
         sys.exit(1)
-    if not is_code_compliant(sys.argv[1]):
-        exit(1)
+
+    # Determines the type of check based on the passed argument
+    check_type = "temp-fix" if sys.argv[1] == "temp-fix" else "compliance"
+    check_function_name = None if sys.argv[1] == "temp-fix" else sys.argv[1]
+
+    # Calls the checking function and exits with error if non-compliant
+    if not is_code_compliant(check_type, check_function_name):
+        sys.exit(1)
