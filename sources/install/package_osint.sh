32f59ed0ab0bf1497f8c47003a6c2bce6d7baf64
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2024-08-04 12:50:14 +0200

    adding aiodns lib for finalrecon

--- sources/install/package_osint.sh
+++ sources/install/package_osint.sh
@@ -339,6 +339,8 @@ function install_finalrecon() {
     python3 -m venv --system-site-packages ./venv
     source ./venv/bin/activate
     pip3 install -r requirements.txt
+    # https://github.com/ThePorgs/Exegol-images/issues/372
+    pip3 install aiodns
     deactivate
     add-aliases finalrecon
     add-history finalrecon
