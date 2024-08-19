0a9d2e3051130160eb1a07a1f939b82e36ebf24e
QU35T-code <github@qu35t-mail.simplelogin.com>
2024-08-19 19:47:02 +0200

    Fix useless cme file

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -1220,7 +1220,6 @@ function install_netexec() {
     mkdir -p ~/.nxc
     [[ -f ~/.nxc/nxc.conf ]] && mv ~/.nxc/nxc.conf ~/.nxc/nxc.conf.bak
     cp -v /root/sources/assets/netexec/nxc.conf ~/.nxc/nxc.conf
-    cp -v /root/sources/assets/grc/conf.cme /usr/share/grc/conf.cme
     add-aliases netexec
     add-history netexec
     add-test-command "netexec --help"
