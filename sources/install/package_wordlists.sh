ecf19aaed987448fa829bfc9b5114a46347a45a7
Ted Kouhouenou <ted.kouhouenou@epitech.eu>
2024-10-20 15:21:12 +0100

    Replace links by raw links

--- sources/install/package_wordlists.sh
+++ sources/install/package_wordlists.sh
@@ -121,19 +121,19 @@ function install_rules(){
     wget https://github.com/NSAKEY/nsa-rules/blob/master/_NSAKEY.v2.dive.rule -P /opt/rules/
     wget https://github.com/praetorian-inc/Hob0Rules/blob/master/d3adhob0.rule -P /opt/rules/
     wget https://github.com/praetorian-inc/Hob0Rules/blob/master/hob064.rule -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/hashesorg.v6/pantagrule.hashorg.v6.hybrid.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/hashesorg.v6/pantagrule.hashorg.v6.one.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/hashesorg.v6/pantagrule.hashorg.v6.popular.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/hashesorg.v6/pantagrule.hashorg.v6.random.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/hashesorg.v6/pantagrule.hashorg.v6.raw1m.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.hashorg.royce/pantagrule.hybrid.royce.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.hashorg.royce/pantagrule.one.royce.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.hashorg.royce/pantagrule.popular.royce.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.hashorg.royce/pantagrule.random.royce.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.v5/pantagrule.private.v5.one.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.v5/pantagrule.private.v5.popular.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.v5/pantagrule.private.v5.random.rule.gz -P /opt/rules/
-    wget https://github.com/rarecoil/pantagrule/tree/master/rules/private.v5/pantagrule.private.v5.hybrid.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.hybrid.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.one.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.popular.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.random.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.raw1m.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.raw1m.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/hashesorg.v6/pantagrule.hashorg.v6.raw1m.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.hashorg.royce/pantagrule.popular.royce.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.hashorg.royce/pantagrule.popular.royce.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.v5/pantagrule.private.v5.hybrid.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.v5/pantagrule.private.v5.hybrid.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.v5/pantagrule.private.v5.popular.rule.gz -P /opt/rules/
+    wget https://github.com/rarecoil/pantagrule/raw/refs/heads/master/rules/private.v5/pantagrule.private.v5.random.rule.gz -P /opt/rules/
 }
 
 
