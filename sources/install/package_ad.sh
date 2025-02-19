8dad4ca84cd32148e67dd13a81166875b857b5ea
compass-dexter <145257008+compass-dexter@users.noreply.github.com>
2025-02-19 14:01:52 +0100

    fix: make fix for road2timeline plugin temporary

--- sources/install/package_ad.sh
+++ sources/install/package_ad.sh
@@ -1197,8 +1197,16 @@ function install_noPac() {
 function install_roadrecon() {
     # CODE-CHECK-WHITELIST=add-aliases,add-history
     colorecho "Installing roadrecon"
-    pipx install --system-site-packages roadrecon
-    pipx inject roadrecon pyyaml numpy pandas # dependencies of road2timeline plugin
+    # Install dependencies of road2timeline plugin
+    # see https://github.com/dirkjanm/ROADtools/pull/121
+    local temp_fix_limit="2025-04-01"
+    if [[ "$(date +%Y%m%d)" -gt "$(date -d $temp_fix_limit +%Y%m%d)" ]]; then
+      criticalecho "Temp fix expired. Exiting."
+      pipx install --system-site-packages roadrecon[road2timeline]
+    else
+      pipx install --system-site-packages roadrecon
+      pipx inject roadrecon pyyaml numpy pandas
+    fi
     add-test-command "roadrecon --help"
     add-test-command "roadrecon-gui --help"
     add-to-list "ROADrecon,https://github.com/dirkjanm/ROADtools#roadrecon,Azure AD recon for red and blue."
