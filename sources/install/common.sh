13c35e8f1cdf95a36943b891fe32b2df3c531da5
Skyf0l <59019720+skyf0l@users.noreply.github.com>
2025-03-03 21:55:39 +0100

    perf: purge cache between layers (#448)
    * Add rust cache purge
    
    * Add npm cache purge
    
    * chore: reduce cleaning verbose
    
    * nvm + bundle
    
    ---------
    
    Co-authored-by: qu35t-code <51704860+qu35t-code@users.noreply.github.com>

--- sources/install/common.sh
+++ sources/install/common.sh
@@ -151,13 +151,18 @@ done
 function post_install() {
     # Function used to clean up post-install files
     colorecho "Cleaning..."
-    local listening_processes
     updatedb
-    rm -rfv /tmp/*
-    rm -rfv /var/lib/apt/lists/*
-    rm -rfv /root/.cache
-    rm -rfv /root/.gradle/caches
+    rm -rf /root/.bundle/cache
+    rm -rf /root/.cache
+    rm -rf /root/.cargo/registry
+    rm -rf /root/.gradle/caches
+    rm -rf /root/.npm/_cacache
+    rm -rf /root/.nvm/.cache
+    rm -rf /tmp/*
+    rm -rf /var/lib/apt/lists/*
+
     colorecho "Stop listening processes"
+    local listening_processes
     listening_processes=$(ss -lnpt | awk -F"," 'NR>1 {split($2,a,"="); print a[2]}')
     if [[ -n $listening_processes ]]; then
         echo "Listening processes detected"
